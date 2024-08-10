4382ca42c611ad235a641f4015790ef3ab070c9d
Alanoud Altuwaijri <alanoud.36449@gmail.com>
2024-08-10 20:28:41 +0300

    Revert "update"
    This reverts commit 9464b82b4ab92c9def7fac0a7a8a43b909f9bf9c.

--- APIs/CSTS.DAL/DTOs/TicketDTO.cs
+++ APIs/CSTS.DAL/DTOs/TicketDTO.cs
@@ -1,6 +1,10 @@
 ﻿// TicketDTO.cs
 using CSTS.DAL.Enum;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CSTS.DAL.DTOs
 {
