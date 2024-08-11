34f3116d416961f520da065068f4f34752afc973
HamzaDevnet <hamzaenim443@gmail.com>
2024-08-11 18:34:55 +0300

    SeedData

--- APIs/CSTS.API/Program.cs
+++ APIs/CSTS.API/Program.cs
@@ -47,6 +47,18 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        SeedData.Initialize(services);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred seeding the DB: {ex.Message}");
+    }
+}
 
 app.UseStaticFiles();
 
