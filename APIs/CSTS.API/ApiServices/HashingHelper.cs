818f065ebf59ab69728ef9fd8dda1ad724959dd7
Ahmad Alkaraki <a.alkaraki@t2.sa>
2024-08-18 12:18:16 +0300

    hashing

---
+++ APIs/CSTS.API/ApiServices/HashingHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CSTS.API.ApiServices
+{
+    public static class HashingHelper
+    {
+        private static string salt = "Improve-security-add-salt";
+
+        public static byte[] GetHash(string inputString)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(string.Concat(salt, inputString)));
+        }
+
+
+        public static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(string.Concat(salt, inputString)))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
+        }
+
+
+        public static bool CompareHash(string attemptedPassword, string hashed)
+        {
+            string base64Hash = Convert.ToBase64String(hashed);
+            string base64AttemptedHash = Convert.ToBase64String(GetHashString(string.Concat(salt, attemptedPassword)));
+
+            return base64Hash == base64AttemptedHash;
+        }
+
+    }
+}
--- APIs/CSTS.API/Controllers/UsersController.cs
+++ APIs/CSTS.API/Controllers/UsersController.cs
@@ -11,6 +11,7 @@ using CSTS.API.ApiServices;
 using CSTS.DAL.AutoMapper.DTOs;
 using AutoMapper;
 using BCrypt.Net;
+using Microsoft.AspNetCore.Identity.Data;
 
 
 namespace CSTS.API.Controllers
@@ -172,38 +173,38 @@ namespace CSTS.API.Controllers
             }
         }
         [HttpPut("{id}/reset-password")]
-        [CstsAuth(UserType.SupportTeamMember, UserType.ExternalClient)]
-        public async Task<ActionResult<APIResponse<bool>>> ResetPassword([FromRoute] Guid id, [FromBody] ResetPasswordDto resetPasswordDto)
+        [CstsAuth(UserType.SupportTeamMember, UserType.ExternalClient, UserType.SupportManager)]
+        public async Task<ActionResult<APIResponse<bool>>> ResetPassword([FromBody] ResetPasswordDto resetPasswordDto)
         {
             try
             {
-                var existingUser = _unitOfWork.Users.GetById(id);
-                if (existingUser == null)
+                Guid id = this.GetCurrentUserId();
+                var user = _unitOfWork.Users.GetById(id);
+
+                if (user == null)
                 {
                     return Ok(new APIResponse<bool> { Data = false, Code = ResponseCode.Null, Message = "User not found." });
                 }
 
-                bool isPasswordValid;
-
-                if (existingUser.Password.StartsWith("$2a$") || existingUser.Password.StartsWith("$2b$") || existingUser.Password.StartsWith("$2y$"))
+                if (user.UserStatus == UserStatus.Deactivated)
                 {
-                    isPasswordValid = BCrypt.Net.BCrypt.Verify(resetPasswordDto.OldPassword, existingUser.Password);
-                }
-                else
-                {
-                    isPasswordValid = existingUser.Password == resetPasswordDto.OldPassword;
+                    return Ok(new APIResponse<bool>(false, "User is Deactivated."));
                 }
 
-                if (!isPasswordValid)
+                if (HashingHelper.CompareHash(resetPasswordDto.OldPassword, user.Password))
                 {
-                    return Ok(new APIResponse<bool> { Data = false, Code = ResponseCode.Error, Message = "Old password is incorrect." });
+                    return Ok(new APIResponse<bool>(false, "Incorrect password."));
+                }
+                
+                if (resetPasswordDto.NewPassword.Count() < 8 )
+                {
+                    return Ok(new APIResponse<bool>(false, "Password at least 8 characters"));
                 }
 
-                string hashedNewPassword = BCrypt.Net.BCrypt.HashPassword(resetPasswordDto.NewPassword);
-                existingUser.Password = hashedNewPassword;
-                var response = _unitOfWork.Users.Update(existingUser);
+                user.Password = HashingHelper.GetHashString(resetPasswordDto.NewPassword);
+                var response = _unitOfWork.Users.Update(user);
 
-                return Ok(new APIResponse<bool> { Data = response, Code = ResponseCode.Success, Message = "Password reset successfully." });
+                return Ok(new APIResponse<bool> { Data = response, Code = ResponseCode.Success, Message = "Password changed successfully." });
             }
             catch (Exception ex)
             {
