e81de7b259a7f89d2338b492f1bb677d4020f064
HamzaDevnet <hamzaenim443@gmail.com>
2024-08-19 15:17:08 +0300

    Migration LastUpdate

---
+++ APIs/CSTS.DAL/Migrations/20240819121515_LastUpdate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSTS.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class LastUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: new Guid("725c2c65-2fc0-45ec-b28f-fc56f268c225"),
+                column: "Image",
+                value: "/Images/Manager.webp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: new Guid("725c2c65-2fc0-45ec-b28f-fc56f268c225"),
+                column: "Image",
+                value: "/Image/Manager.webp");
+        }
+    }
+}
